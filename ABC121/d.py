8a301748dbf815851979e5e91470ba64c3fdf52c
SJSY <soya.saijo@gmail.com>
2019-03-10 09:31:11 +0900

    add ABC121

--- ABC121/d.py
+++ ABC121/d.py
@@ -1,7 +1,20 @@
+def f(n):
+    x = n // 2
+
+    if n % 2 == 0:
+        if x % 2 == 0:
+            return 0 ^ n
+        else:
+            return 1 ^ n
+
+    else:
+        x += 1
+        if x % 2 == 0:
+            return 0
+        else:
+            return 1
+
+
 A, B = map(int, input().split())
 
-ans = A
-
-for i in range(A+1, B+1):
-    ans = ans ^ i
-    print(i, ans, bin(ans))
+print(f(A-1) ^ f(B))
